0517c3d29672450d7e8efec526fdc090f5c94de4
KIlnur <kilnur1988@rambler.ru>
2023-09-13 14:30:29 +0500

    Выделил компонент Контакты. Закончил разбивку главной страницы на компоненты

--- maroon-react/src/components/pages/mainScreen/mainScreen.tsx
+++ maroon-react/src/components/pages/mainScreen/mainScreen.tsx
@@ -1,4 +1,5 @@
 import Bestsellers from '../../blocks/bestsellers/bestsellers';
+import Contacts from '../../blocks/contacts/contacts';
 import Intro from '../../blocks/intro/intro';
 import SocialList from '../../blocks/social-list/social-list';
 
@@ -35,7 +36,7 @@ function MainScreen(): JSX.Element {
       <section className="subscribe">
         <h2 className="subscribe__title">Присоединяйтесь к нам</h2>
         <p className="subscribe__slogan">Подпишитесь на наш аккаунт @marooncare и узнавайте о новиках и акциях первыми</p>
-        <button className="subscribe__btn">Подписаться</button>
+        <button className="subscribe__btn button">Подписаться</button>
         <ul className="subscribe__image-list">
           <li className="subscribe__image-item">
             <img src="" alt="" className="subscribe__image" />
@@ -58,27 +59,7 @@ function MainScreen(): JSX.Element {
         </ul>
       </section>
 
-      <section className="contacts">
-        <h2 className="contacts__title">Контакты</h2>
-        <ul className="contacts__list">
-          <li className="contacts__item">
-            <h3 className="contacts__title">Адрес</h3>
-            <p className="contacts__desc">
-              Санкт-Петербург,ул. Большая Конюшенная, 19
-            </p>
-          </li>
-          <li className="contacts__item">
-            <h3 className="contacts__title">Телефон</h3>
-            <p className="contacts__desc">+7 (923) 888-90-60</p>
-          </li>
-          <li className="contacts__item">
-            <h3 className="contacts__title">E-mail</h3>
-            <p className="contacts__desc">info@maroon.ru</p>
-          </li>
-        </ul>
-        <SocialList />
-        <div className="contacts__map-wrapper"></div>
-      </section>
+      <Contacts />
 
     </main>
   );
