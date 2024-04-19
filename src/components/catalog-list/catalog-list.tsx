47535ef2f98e4674e058e9fec73bd3fb00612c45
KIlnur <kilnur1988@rambler.ru>
2024-04-19 14:11:53 +0500

    Реализует пагинацию. WIP(Неверно отображает количество продуктов на странице из-за неудачно выбранной структуры данных)

--- src/components/catalog-list/catalog-list.tsx
+++ src/components/catalog-list/catalog-list.tsx
@@ -4,7 +4,8 @@ import { getAllProducts, getAllProductsLoadingStatus } from '../../store/product
 import Pagination from '../pagination/pagination';
 import ProductCardSmall from '../product-card-small/product-card-small';
 import Spinner from '../spinner/spinner';
-import { CATALOG_PER_PAGE_COUNT } from '../../const';
+import { CATALOG_PER_PAGE_COUNT, SIDE_CATALOG_PER_PAGE_COUNT } from '../../const';
+import { Product } from '../../types/product';
 
 type Props = {
   catalogType?: string;
@@ -15,26 +16,54 @@ function CatalogList({ catalogType }: Props): JSX.Element {
   const isProductsLoading = useAppSelector(getAllProductsLoadingStatus);
 
   const [offset, setOffset] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [page, setPage] = useState(1);
   const [pageCount, setPageCount] = useState(0);
+  const [currentProducts, setCurrentProducts] = useState<Product[]>([]);
+  const [itemsPerPage, setItemsPerPage] = useState(
+    (!catalogType ? CATALOG_PER_PAGE_COUNT : SIDE_CATALOG_PER_PAGE_COUNT)
+  );
 
   useEffect(() => {
-    if (products) { setPageCount(Math.ceil(products.length / CATALOG_PER_PAGE_COUNT)); }
-  }, [])
+    (!catalogType)
+      ? setItemsPerPage(CATALOG_PER_PAGE_COUNT)
+      : setItemsPerPage(SIDE_CATALOG_PER_PAGE_COUNT);
+
+    if (products) {
+      setPageCount(Math.ceil(products.length / itemsPerPage));
+      setCurrentProducts(products.slice(offset, offset + itemsPerPage));
+    }
+
+    console.log(currentProducts);
+  }, [products, offset, itemsPerPage, catalogType])
+
+  const handlePrevBtnClick = (currentPage: number): void => {
+    setPage(currentPage - 1);
+    setOffset((currentPage - 1) * itemsPerPage);
+  };
+
+  const handleNextBtnClick = (currentPage: number): void => {
+    setPage(currentPage + 1);
+    setOffset(currentPage * itemsPerPage);
+  }
 
   return (
     <>
       <ul className={`catalog${catalogType ? '-history' : ''}__list`}>
         {
-          products &&
-          products.map((product, index) => (
+          currentProducts &&
+          currentProducts.map((product, index) => (
             <ProductCardSmall product={product} className={`catalog${catalogType ? '-history' : ''}__item`} key={`${index}${product.name}${product.id}`} />
           ))}
         {
           isProductsLoading && <Spinner />
         }
       </ul>
-      <Pagination />
+      <Pagination
+        page={page}
+        pageCount={pageCount}
+        handleNextBtnClick={handleNextBtnClick}
+        handlePrevBtnClick={handlePrevBtnClick}
+      />
     </>
   );
 }
--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,27 +1,42 @@
 
-function Pagination(): JSX.Element {
+type Props = {
+  page: number,
+  pageCount: number,
+  handleNextBtnClick(currentPage: number): void,
+  handlePrevBtnClick(currentPage: number): void
+}
+
+function Pagination({
+  page,
+  pageCount,
+  handleNextBtnClick,
+  handlePrevBtnClick
+}: Props): JSX.Element {
+
+  const currentPage = page;
+
   return (
     <div className="pagination">
       <div className="pagination__numbers">
         <span className="pagination__number">
           <a className="pagination__number-link pagination__number-link_current" href="#"
             aria-label="Первая страница">
-            1
+            {currentPage}
           </a>
         </span>
         <span className="pagination__number">
           <a className="pagination__number-link" href="#" aria-label="Восьмая страница (последняя)">
-            8
+            {pageCount}
           </a>
         </span>
       </div>
       <div className="pagination__arrows">
-        <a className="pagination__arrow" href="#" aria-label="Предыдущая страница">
+        <a className="pagination__arrow" href="#" aria-label="Предыдущая страница" onClick={() => handlePrevBtnClick(currentPage)}>
           <svg className="slider-icon" height="12" width="26">
             <use xlinkHref="#arrow-left"></use>
           </svg>
         </a>
-        <a className="pagination__arrow" href="#" aria-label="Следующая страница">
+        <a className="pagination__arrow" href="#" aria-label="Следующая страница" onClick={() => handleNextBtnClick(currentPage)}>
           <svg className="slider-icon" height="12" width="26">
             <use xlinkHref="#main-arrow"></use>
           </svg>
